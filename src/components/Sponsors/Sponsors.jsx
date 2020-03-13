f2e7b8714e56c0bd96c96b6068d2434dee076b25
Katja Süss <k.suess@rohberg.ch>
2020-03-13 15:48:15 +0100

    Sponsors component: fetching sponsors: call action
    existing action  fetches content via rest api

--- src/components/Sponsors/Sponsors.jsx
+++ src/components/Sponsors/Sponsors.jsx
@@ -4,6 +4,8 @@
  */
 
 import React from 'react';
+import { searchContent } from '@plone/volto/actions';
+import { useDispatch, useSelector } from 'react-redux';
 import { Container, List, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
@@ -14,14 +16,39 @@ import { FormattedMessage, defineMessages, injectIntl } from 'react-intl';
  * @param {Object} intl Intl object
  * @returns {string} Markup of the component
  */
-const Sponsors = ({ intl }) => (
-  <Segment vertical padded>
-    <Container>
-      <Segment basic>
-        <h3>We ❤ our sponsors</h3>
-      </Segment>
-    </Container>
-  </Segment>
-);
 
-export default injectIntl(Sponsors);
+const Sponsors = props => {
+  const searchSubrequests = useSelector(state => state.search);
+  const dispatch = useDispatch();
+  const results = searchSubrequests?.items;
+
+  React.useEffect(() => {
+    dispatch(
+      searchContent('/', {
+        sort_on: 'created',
+        metadata_fields: '_all',
+        portal_type: ['sponsor'],
+      }),
+    );
+  }, [dispatch]);
+
+  return (
+    <Segment vertical padded>
+      <Container>
+        <Segment basic>
+          <h3>We ❤ our sponsors</h3>
+          <ul>
+            {results &&
+              results.map(sponsor => (
+                <li key={sponsor['@id']}>
+                  <Link to={sponsor['@id']}>{sponsor.title}</Link>
+                </li>
+              ))}
+          </ul>
+        </Segment>
+      </Container>
+    </Segment>
+  );
+};
+
+export default Sponsors;
