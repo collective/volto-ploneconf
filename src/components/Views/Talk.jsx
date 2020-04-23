93132d91c1f030558dd5d42a30b17be55c597aa3
Philip Bauer <bauer@starzel.de>
2020-04-23 11:25:22 +0200

    udpate talkview to show dates

--- src/components/Views/Talk.jsx
+++ src/components/Views/Talk.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { flattenToAppURL } from '@plone/volto/helpers';
-import { Container, Image, Icon, Label, Segment } from 'semantic-ui-react';
+import {
+  Container,
+  Header,
+  Image,
+  Icon,
+  Label,
+  Segment,
+} from 'semantic-ui-react';
 import { Helmet } from '@plone/volto/helpers';
 import { When } from '@plone/volto/components/theme/View/EventDatesInfo';
 
@@ -9,7 +16,7 @@ const TalkView = props => {
   const color_mapping = {
     Beginner: 'green',
     Advanced: 'yellow',
-    Professional: 'red',
+    Professionals: 'red',
   };
 
   return (
@@ -19,80 +26,101 @@ const TalkView = props => {
         {content.type_of_talk.title || content.type_of_talk.token}:{' '}
         {content.title}
       </h1>
+      <Segment floated="right">
+        {content.start && !content.hide_date && (
+          <>
+            <Header dividing sub>
+              When
+            </Header>
+            <When
+              start={content.start}
+              end={content.end}
+              whole_day={content.whole_day}
+              open_end={content.open_end}
+            />
+          </>
+        )}
+        {content.room && (
+          <>
+            <Header dividing sub>
+              Where
+            </Header>
+            <p>{content.room.title}</p>
+          </>
+        )}
+        {content.audience && (
+          <Header dividing sub>
+            Audience
+          </Header>
+        )}
+        {content.audience.map(item => {
+          let audience = item.title || item.token;
+          let color = color_mapping[audience] || 'green';
+          return (
+            <Label key={audience} color={color}>
+              {audience}
+            </Label>
+          );
+        })}
+      </Segment>
       {content.description && (
         <p className="documentDescription">{content.description}</p>
       )}
-      {content.audience.map(item => {
-        let audience = item.title || item.token;
-        let color = color_mapping[audience] || 'green';
-        return (
-          <Label key={audience} color={color}>
-            {audience}
-          </Label>
-        );
-      })}
-      {content.start && !content.hide_date && (
-        <When
-          start={content.start}
-          end={content.end}
-          whole_day={content.whole_day}
-          open_end={content.open_end}
-        />
-      )}
-      {content.room && <p>Location: {content.room.title}</p>}
       {content.details && (
         <div dangerouslySetInnerHTML={{ __html: content.details.data }} />
       )}
-      <Segment clearing>
-        {content.speaker && <h3>{content.speaker}</h3>}
-        {content.website ? (
-          <p>
-            <a href={content.website}>{content.company}</a>
-          </p>
-        ) : (
-          <p>{content.company}</p>
-        )}
-        {content.email && (
-          <p>
-            <a href={`mailto:${content.email}`}>
-              <Icon name="mail" /> {content.email}
-            </a>
-          </p>
-        )}
-        {content.twitter && (
-          <p>
-            <a href={`https://twitter.com/${content.twitter}`}>
-              <Icon name="twitter" />{' '}
-              {content.twitter.startsWith('@')
-                ? content.twitter
-                : '@' + content.twitter}
-            </a>
-          </p>
-        )}
-        {content.github && (
-          <p>
-            <a href={`https://github.com/${content.github}`}>
-              <Icon name="github" /> {content.github}
-            </a>
-          </p>
-        )}
-        {content.image && (
-          <Image
-            src={flattenToAppURL(content.image.scales.preview.download)}
-            size="small"
-            floated="right"
-            alt={content.image_caption}
-            avatar
-          />
-        )}
-        {content.speaker_biography && (
-          <div
-            dangerouslySetInnerHTML={{
-              __html: content.speaker_biography.data,
-            }}
-          />
-        )}
-      </Segment>
+      {content.speaker && (
+        <Segment clearing>
+          <Header dividing>{content.speaker}</Header>
+          {content.website ? (
+            <p>
+              <a href={content.website}>{content.company}</a>
+            </p>
+          ) : (
+            <p>{content.company}</p>
+          )}
+          {content.email && (
+            <p>
+              <a href={`mailto:${content.email}`}>
+                <Icon name="mail" /> {content.email}
+              </a>
+            </p>
+          )}
+          {content.twitter && (
+            <p>
+              <a href={`https://twitter.com/${content.twitter}`}>
+                <Icon name="twitter" />{' '}
+                {content.twitter.startsWith('@')
+                  ? content.twitter
+                  : '@' + content.twitter}
+              </a>
+            </p>
+          )}
+          {content.github && (
+            <p>
+              <a href={`https://github.com/${content.github}`}>
+                <Icon name="github" /> {content.github}
+              </a>
+            </p>
+          )}
+          {content.image && (
+            <Image
+              src={flattenToAppURL(content.image.scales.preview.download)}
+              size="small"
+              floated="right"
+              alt={content.image_caption}
+              avatar
+            />
+          )}
+          {content.speaker_biography && (
+            <div
+              dangerouslySetInnerHTML={{
+                __html: content.speaker_biography.data,
+              }}
+            />
+          )}
+        </Segment>
+      )}
     </Container>
   );
 };
