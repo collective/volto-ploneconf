431cd5aa13add5a62635dba1672cee99bb9b4c39
Katja Suess <k.suess@rohberg.ch>
2020-11-18 09:53:02 +0100

    Update content.js

--- cypress/integration/content.js
+++ cypress/integration/content.js
@@ -1,6 +1,6 @@
-describe('Add Content Tests', () => {
+describe('Add talk tests', () => {
   beforeEach(() => {
-    // give a logged in editor and the site root
+    // given a logged in editor and the site root
     cy.autologin();
     cy.visit('/');
     cy.waitForResourceToLoad('@navigation');
@@ -10,22 +10,26 @@ describe('Add Content Tests', () => {
     cy.waitForResourceToLoad('?fullobjects');
   });
   it('As editor I can add a talk.', function () {
-    // when I add a talk
+    // when I add a talk with title, type and details
     cy.get('#toolbar-add').click();
-    // cy.get('#toolbar-add-talk-item').click();
-    // cy.get('input[name="title"]')
-    //   .type('Security in Plone')
-    //   .should('have.value', 'Security in Plone');
-    // cy.get('input[name="type_of_talk"]')
-    //   .check('Talk')
-    //   .should('have.value', 'Talk');
-    // cy.get('input[name="details"]')
-    //   .type('description of the talk')
-    //   .should('have.value', 'description of the talk');
-    // cy.get('#toolbar-save').click();
+    cy.get('#toolbar-add-talk').click();
+    cy.get('input[name="title"]')
+      .type('Respect')
+      .should('have.value', 'Respect');
+    cy.get(
+      '#default-type_of_talk .react-select-container > .react-select__control .icon',
+    )
+      .click()
+      .type('Talk{enter}');
+    cy.get('#default-details .public-DraftEditor-content')
+      .type('Mention your co workers.')
+      .get('span[data-text]')
+      .contains('Mention your co workers.');
+    cy.get('#toolbar-save').click();
 
-    // // then a new talk should have been created
-    // cy.url().should('eq', Cypress.config().baseUrl + '/security-in-plone');
-    // cy.get('.navigation .item.active').should('have.text', 'Security in Plone');
+    // then a new talk should have been created
+    cy.url().should('eq', Cypress.config().baseUrl + '/respect');
+    cy.get('body').contains('Respect');
+    cy.get('body').contains('Mention your co workers.');
   });
 });
