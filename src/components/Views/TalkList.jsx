e1bdb658b8bca1a3ed66ca0caab4fe26b5c066e6
Philip Bauer <bauer@starzel.de>
2020-12-03 21:39:51 +0100

    Add talklist_view

--- src/components/Views/TalkList.jsx
+++ src/components/Views/TalkList.jsx
@@ -1,6 +1,80 @@
 import React from 'react';
+import { Container, Segment, Label, Image } from 'semantic-ui-react';
+import { Helmet } from '@plone/volto/helpers';
+import { Link } from 'react-router-dom';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { searchContent } from '@plone/volto/actions';
+import { useDispatch, useSelector } from 'react-redux';
 
 const TalkListView = (props) => {
-  return <div>I'm the TalkList component!</div>;
+  const { content } = props;
+  const searchRequests = useSelector((state) => state.search);
+  const dispatch = useDispatch();
+  const results = searchRequests.items;
+
+  const color_mapping = {
+    Beginner: 'green',
+    Advanced: 'yellow',
+    Professional: 'red',
+  };
+
+  React.useEffect(() => {
+    dispatch(
+      searchContent('/', {
+        portal_type: ['talk'],
+        fullobjects: true,
+      }),
+    );
+  }, [dispatch]);
+
+  return (
+    <Container className="view-wrapper">
+      <Helmet title={content.title} />
+      <article id="content">
+        <header>
+          <h1 className="documentFirstHeading">{content.title}</h1>
+          {content.description && (
+            <p className="documentDescription">{content.description}</p>
+          )}
+        </header>
+        <section id="content-core">
+          {results &&
+            results.map((item) => (
+              <Segment padded>
+                <h2>
+                  <Link to={item['@id']} title={item['@type']}>
+                    {item.type_of_talk.title || item.type_of_talk.token}:{' '}
+                    {item.title}
+                  </Link>
+                </h2>
+                {item.audience.map((item) => {
+                  let audience = item.title || item.token;
+                  let color = color_mapping[audience] || 'green';
+                  return (
+                    <Label key={audience} color={color}>
+                      {audience}
+                    </Label>
+                  );
+                })}
+                {item.image && (
+                  <Image
+                    src={flattenToAppURL(item.image.scales.preview.download)}
+                    size="small"
+                    floated="right"
+                    alt={content.image_caption}
+                    avatar
+                  />
+                )}
+                {item.description && <div>{item.description}</div>}
+                <Link to={item['@id']} title={item['@type']}>
+                  read more ...
+                </Link>
+              </Segment>
+            ))}
+        </section>
+      </article>
+    </Container>
+  );
 };
+
 export default TalkListView;
