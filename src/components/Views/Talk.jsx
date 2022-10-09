838799b3f4842bdc79391af2c257cee9f3501666
Katja Süss <k.suess@rohberg.ch>
2022-10-09 18:52:18 +0200

    Add talk view

---
+++ src/components/Views/Talk.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import { flattenToAppURL } from '@plone/volto/helpers';
+import { Container, Image, Segment } from 'semantic-ui-react';
+
+const TalkView = ({ content }) => {
+  return (
+    <Container id="view-wrapper talk-view">
+      <h1 className="documentFirstHeading">
+        {content.type_of_talk.title || content.type_of_talk.token}:{' '}
+        {content.title}
+      </h1>
+      {content.description && (
+        <p className="documentDescription">{content.description}</p>
+      )}
+      {content.details && (
+        <div dangerouslySetInnerHTML={{ __html: content.details.data }} />
+      )}
+      <Segment clearing>
+        {content.speaker && <h3>{content.speaker}</h3>}
+        {content.website ? (
+          <p>
+            <a href={content.website}>{content.company}</a>
+          </p>
+        ) : (
+          <p>{content.company}</p>
+        )}
+        {content.email && (
+          <p>
+            Email: <a href={`mailto:${content.email}`}>{content.email}</a>
+          </p>
+        )}
+        {content.twitter && (
+          <p>
+            Twitter:{' '}
+            <a href={`https://twitter.com/${content.twitter}`}>
+              {content.twitter.startsWith('@')
+                ? content.twitter
+                : '@' + content.twitter}
+            </a>
+          </p>
+        )}
+        {content.github && (
+          <p>
+            Github:{' '}
+            <a href={`https://github.com/${content.github}`}>
+              {content.github}
+            </a>
+          </p>
+        )}
+        {content.image && (
+          <Image
+            src={flattenToAppURL(content.image.scales.preview.download)}
+            size="small"
+            floated="right"
+            alt={content.image_caption}
+            avatar
+          />
+        )}
+        {content.speaker_biography && (
+          <div
+            dangerouslySetInnerHTML={{
+              __html: content.speaker_biography.data,
+            }}
+          />
+        )}
+      </Segment>
+    </Container>
+  );
+};
+export default TalkView;
