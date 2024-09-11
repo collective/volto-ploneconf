e53b44355eb1aa06b006e31cf77a7f0829c82719
Katja Süss <k.suess@rohberg.ch>
2024-09-11 20:54:40 +0200

    Title, description

--- packages/volto-ploneconf/src/components/Views/Talk.jsx
+++ packages/volto-ploneconf/src/components/Views/Talk.jsx
@@ -1,17 +1,21 @@
 import { Container as SemanticContainer } from 'semantic-ui-react';
-import { DefaultView } from '@plone/volto/components';
 import config from '@plone/volto/registry';
 
 const TalkView = (props) => {
+  const { content } = props;
   const Container =
     config.getComponent({ name: 'Container' }).component || SemanticContainer;
   return (
-    <>
-      <DefaultView {...props} />
-      <Container>
-        <div dangerouslySetInnerHTML={{ __html: props.content.details.data }} />
-      </Container>
-    </>
+    <Container id="view-wrapper talk-view">
+      <h1 className="documentFirstHeading">
+        <span className="type_of_talk">{content.type_of_talk.token}: </span>
+        {content.title}
+      </h1>
+      {content.description && (
+        <p className="documentDescription">{content.description}</p>
+      )}
+      <div dangerouslySetInnerHTML={{ __html: content.details.data }} />
+    </Container>
   );
 };
 export default TalkView;
