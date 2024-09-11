76f591ab9565788941d39e910d927e54dace633b
Katja Süss <k.suess@rohberg.ch>
2024-09-11 20:25:20 +0200

    Use DefaultView and show value of field 'details'

--- packages/volto-ploneconf/src/components/Views/Talk.jsx
+++ packages/volto-ploneconf/src/components/Views/Talk.jsx
@@ -1,5 +1,17 @@
-const TalkView = (props) => {
-  return <div>I am the TalkView component!</div>;
-};
+import { Container as SemanticContainer } from 'semantic-ui-react';
+import { DefaultView } from '@plone/volto/components';
+import config from '@plone/volto/registry';
 
+const TalkView = (props) => {
+  const Container =
+    config.getComponent({ name: 'Container' }).component || SemanticContainer;
+  return (
+    <>
+      <DefaultView {...props} />
+      <Container>
+        <div dangerouslySetInnerHTML={{ __html: props.content.details.data }} />
+      </Container>
+    </>
+  );
+};
 export default TalkView;
