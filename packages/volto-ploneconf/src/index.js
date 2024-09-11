d15cc68945f0631411534a2e98c4ca1b94653ad4
Katja Süss <k.suess@rohberg.ch>
2024-09-11 19:52:00 +0200

    Add view for talk

--- packages/volto-ploneconf/src/index.js
+++ packages/volto-ploneconf/src/index.js
@@ -1,4 +1,13 @@
+import { TalkView } from './components';
+
 const applyConfig = (config) => {
+  config.views = {
+    ...config.views,
+    contentTypesViews: {
+      ...config.views.contentTypesViews,
+      talk: TalkView,
+    },
+  };
   return config;
 };
 
