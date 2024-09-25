37acacd1ee46806d500a86d8dadfbbe26ec1b9ba
Katja Süss <k.suess@rohberg.ch>
2024-09-25 10:11:10 +0200

    Add metadata to listing variation

--- packages/volto-ploneconf/src/components/variations/TalkListingBlockVariation.jsx
+++ packages/volto-ploneconf/src/components/variations/TalkListingBlockVariation.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Label, Segment } from 'semantic-ui-react';
 import { ConditionalLink, Component } from '@plone/volto/components';
+import { When } from '@plone/volto/components/theme/View/EventDatesInfo';
 import { flattenToAppURL } from '@plone/volto/helpers';
 
 import { isInternalURL } from '@plone/volto/helpers/Url/Url';
 
-const SummaryTemplate = ({ items, linkTitle, linkHref, isEditMode }) => {
+const TalkListingBlockVariation = ({
+  items,
+  linkTitle,
+  linkHref,
+  isEditMode,
+}) => {
   let link = null;
   let href = linkHref?.[0]?.['@id'] || '';
+  const color_mapping_audience = {
+    beginner: 'green',
+    advanced: 'yellow',
+    professional: 'purple',
+  };
 
   if (isInternalURL(href)) {
     link = (
@@ -23,15 +35,47 @@ const SummaryTemplate = ({ items, linkTitle, linkHref, isEditMode }) => {
     <>
       <div className="items">
         {items.map((item) => (
-          <div className="listing-item" key={item['@id']}>
-            <ConditionalLink item={item} condition={!isEditMode}>
-              <Component componentName="PreviewImage" item={item} alt="" />
-              <div className="listing-body">
-                <h3>{item.title || item.id}</h3>
-                <p>{item.description}</p>
-              </div>
-            </ConditionalLink>
-          </div>
+          <Segment key={item['@id']}>
+            <div className="listing-item">
+              <ConditionalLink item={item} condition={!isEditMode}>
+                <Component componentName="PreviewImage" item={item} alt="" />
+                <div className="listing-body">
+                  <When
+                    start={item.start}
+                    end={item.end}
+                    whole_day={item.whole_day}
+                    open_end={item.open_end}
+                  />
+                  <h3>{item.title || item.id}</h3>
+                  <p>{item.speaker}</p>
+                  <p>
+                    {item.room && (
+                      <>
+                        <b>Room: </b>
+                        {item.room}
+                        <br />
+                      </>
+                    )}
+                    {item.audience?.length > 0 && (
+                      <>
+                        <b>Audience:</b>
+                        {item.audience?.map((audience) => {
+                          let color =
+                            color_mapping_audience[audience] || 'green';
+                          return (
+                            <Label key={audience} color={color}>
+                              {audience}
+                            </Label>
+                          );
+                        })}
+                      </>
+                    )}
+                  </p>
+                  <p>{item.description}</p>
+                </div>
+              </ConditionalLink>
+            </div>
+          </Segment>
         ))}
       </div>
 
@@ -40,10 +84,10 @@ const SummaryTemplate = ({ items, linkTitle, linkHref, isEditMode }) => {
   );
 };
 
-SummaryTemplate.propTypes = {
+TalkListingBlockVariation.propTypes = {
   items: PropTypes.arrayOf(PropTypes.any).isRequired,
   linkMore: PropTypes.any,
   isEditMode: PropTypes.bool,
 };
 
-export default SummaryTemplate;
+export default TalkListingBlockVariation;
